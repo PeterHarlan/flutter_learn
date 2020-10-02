33f54e2570c77190f743314765bb5f4108b442eb
peterjharlan@gmail.com <peterjharlan@gmail.com>
2020-10-02 00:39:05 -0400

    Implement the text section

--- lib/main.dart
+++ lib/main.dart
@@ -44,14 +44,28 @@ class MyApp extends StatelessWidget {
     Color color = Theme.of(context).primaryColor;
 
     Widget buttonSection = Container(
-        child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, "CALL"),
-        _buildButtonColumn(color, Icons.near_me, "ROUTE"),
-        _buildButtonColumn(color, Icons.share, "SHARE"),
-      ],
-    ));
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, "CALL"),
+          _buildButtonColumn(color, Icons.near_me, "ROUTE"),
+          _buildButtonColumn(color, Icons.share, "SHARE"),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
 
     return MaterialApp(
       title: 'Flutter layout demo',
@@ -62,7 +76,7 @@ class MyApp extends StatelessWidget {
         // Add the title section to the app body
         body: Center(
             child: Column(
-          children: [titleSection, buttonSection],
+          children: [titleSection, buttonSection, textSection],
         )),
       ),
     );
