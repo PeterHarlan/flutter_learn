e10b0042638622ff6ba4570916d9c1c0833e7337
peterjharlan@gmail.com <peterjharlan@gmail.com>
2020-10-05 22:47:38 -0400

    Apply flutter update and tap to change color boxes

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: "Flutter demo",
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          appBar: AppBar(
+            title: Text("Flutter Demo"),
+          ),
+          body: Center(
+            child: TapboxA(),
+          )),
+    );
+  }
+}
+
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
+
+  @override
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  // Determins the box's current color and text
+  bool _active = false;
+
+  // updates _active when the box is tapped
+  // Calls setState() to update the UI
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Detects gestures
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            _active ? 'Active' : "Inactive",
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
         ),
-        body: Center(
-          child: Text('Hello World'),
+        width: 200.0,
+        height: 200.0,
+        // Draws either a light green or gray box
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
         ),
       ),
     );
