7d94d4fcec9190cddeff5a54d4deadeb4eb34a6b
peterjharlan@gmail.com <peterjharlan@gmail.com>
2020-10-08 23:29:09 -0400

    Add mix-state demo

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _ParentWidgetState extends State<ParentWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TapboxB(
+      child: TapboxC(
         active: _active,
         onChanged: _handleTapboxChanged,
       ),
@@ -45,34 +45,66 @@ class _ParentWidgetState extends State<ParentWidget> {
   }
 }
 
-class TapboxB extends StatelessWidget {
-  TapboxB({Key key, this.active: false, @required this.onChanged})
-      : super(key: key);
+class TapboxC extends StatefulWidget {
+  TapboxC({Key key, this.active: false, @required this.onChanged});
 
   final bool active;
   final ValueChanged<bool> onChanged;
 
+  @override
+  _TapboxCState createState() => _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  // On tap down, the border appears
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  // On tap up, the border disappears
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
   void _handleTap() {
-    onChanged(!active);
+    widget.onChanged(!widget.active);
   }
 
   @override
   Widget build(BuildContext context) {
+    // This example adds a green border on tap down.
+    // On tap up, the square changes to the opposite state
+    // GestureDetector listens to all tap events
     return GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
       onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
       child: Container(
-        child: Center(
-          child: Text(
-            active ? "Active" : "Inactive",
-            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          child: Center(
+            child: Text(widget.active ? 'Active' : 'Inactive',
+                style: TextStyle(fontSize: 32.0, color: Colors.white)),
           ),
-        ),
-        width: 200.0,
-        height: 200.0,
-        decoration: BoxDecoration(
-          color: active ? Colors.lightGreen[700] : Colors.grey[600],
-        ),
-      ),
+          width: 200.0,
+          height: 200.0,
+          decoration: BoxDecoration(
+            color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+            border: _highlight
+                ? Border.all(color: Colors.teal[700], width: 10.0)
+                : null,
+          )),
     );
   }
 }
